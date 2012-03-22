9dc1549930b8da85bffff49a3e224bf787dc7855
Kenny Luong <luong97@hawaii.edu>
2012-03-22 10:35:49 -1000

    added ignore file and monopoly driver

---
+++ monopoly.cc
@@ -0,0 +1,19 @@
+//	EE205
+//	R4T5
+//	Final Project
+//
+//	Monopoly.cc 
+//	Main Driver for Final Project
+//
+
+#include <iostream>
+using namespace std;
+main()
+{
+
+	cout << "Monopoly!" << endl;
+
+}
+
+
+
