427380895e18d492c28a04289825dac669e12a6d
Bryant Lee <blee4@hawaii.edu>
2012-04-15 18:54:15 -1000

    Placebo for properties.cc

---
+++ properties.cc
@@ -0,0 +1,16 @@
+
+/* File: properties.cc
+ * Name: R4T5
+ * Date: 15 Apr 12
+ * Rev: v0.1
+ */
+
+/* Constructor & Destructor */
+Properties :: Properties()
+{
+	Entry t1;
+	const int t1.owner = 0;	// 0 is banker
+
+	t1.name = "Go";
+	list.insert(t1);
+}
