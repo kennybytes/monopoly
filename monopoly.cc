e49e78be0d1fbf7100024a4b1fc13ba7378fe9d8
Kevin Hu <kevinhu@hawaii.edu>
2012-04-16 16:51:45 -1000

    !

--- monopoly.cc
+++ monopoly.cc
@@ -13,6 +13,33 @@ main()
 
 	cout << "Now we're playing monopoly!" << endl;
 
+
+/*
+ * 	start
+ * 	init
+ * 	player
+ * 		1) roll
+ * 			update pos
+ * 			action block
+ * 			check
+ * 		2) manage properties
+ * 		3) display cash
+ * 		4) trade
+ *		5) if(rolled)
+ *			check
+ *			next player
+ *
+ */
+
+
+
+
+
+
+
+
+
+
 }
 
 
