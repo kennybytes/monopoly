86a719fcc92350bbd03034dd660a3297ebb2a15a
Kenny Luong <luong97@hawaii.edu>
2012-04-21 22:36:33 -1000

    added a test driver for gamespace

---
+++ Properties/testgamespace.cc
@@ -0,0 +1,28 @@
+/*
+ *
+ * Test driver for 'Property' class
+ *
+ */
+
+
+#include <iostream>
+using namespace std;
+
+#include "gamespace.h"
+#include "container.h"
+
+int main()
+{
+
+	GameSpace A;
+	A.setName("Test 1");
+	A.setPrice(100);
+	A.setOwner("Kenny");
+	A.setColor();
+	cout << A << endl;
+	
+	
+
+}
+
+
