497b4ac53c052451115d0a6ab7d7de1c72263cd8
Kenny Luong <luong97@hawaii.edu>
2012-04-21 22:35:32 -1000

    added some colors

---
+++ Properties/color.h
@@ -0,0 +1,35 @@
+/*
+ *
+ * File Name:	color.h
+ * Author:	Kenny Luong
+ * Date:	4/21/12
+ *
+ * Description:
+ *
+ * This file contains the colors for the monopoly board.
+ * 
+ */
+
+
+typedef int Color;
+
+/*
+ * Brown
+ * SkyBlue
+ * Indigo
+ * Orange
+ * Red
+ * Yellow
+ * Green
+ * Blue
+ */
+
+static const Color BROWN = 0;
+static const Color SKYBLUE = 1; 
+static const Color INDIGO = 2;
+static const Color ORANGE = 3;
+static const Color RED = 4;
+static const Color YELLOW = 5;
+static const Color GREEN = 6;
+static const Color BLUE = 7;
+
