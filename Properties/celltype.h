d06029119e50b0178a2b0a0d4bd7e480f441b655
Kenny Luong <luong97@hawaii.edu>
2012-04-21 22:34:59 -1000

    added celltype.h with celltypes

--- Properties/celltype.h
+++ Properties/celltype.h
@@ -1,9 +1,31 @@
-const CellType CHANCE = 0;
-const CellType COMMUNITY_CHEST = 1;
-const CellType UTILITY = 2;
-const CellType PROPERTY = 3
-const CellType TAX = 4;
-const CellType FREE_PARKING = 5;
-const CellType Go_TO_JAIL = 6;
-const CellType JAIL = 7;
 
+/*
+ *
+ *
+ *
+ * Name: celltype.h
+ * Author: Kenny Luong
+ * Date Created: 4/21/12
+ *
+ * Description:
+ *
+ * This file contains the static celltype declarations
+ * 
+ */
+
+#ifndef CELLTYPE_H
+#define CELLTYPE_H
+
+typedef int CellType;
+
+static const CellType NON = -1;
+static const CellType CHANCE = 0;
+static const CellType COMMUNITY_CHEST = 1;
+static const CellType UTILITY = 2;
+static const CellType PROPERTY = 3;
+static const CellType TAX = 4;
+static const CellType FREE_PARKING = 5;
+static const CellType Go_TO_JAIL = 6;
+static const CellType JAIL = 7;
+
+#endif
