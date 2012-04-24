118bf47481c578789d585ad62cf3cb4301a07b26
Kenny Luong <luong97@hawaii.edu>
2012-04-23 15:58:52 -1000

    added the chance folder

---
+++ Chance/chance.h
@@ -0,0 +1,16 @@
+/*
+ *
+ *
+ *	File name: 	chance.h
+ *	Author: 	Kenny Luong
+ *	Date: 		4/24/12
+ *
+ *	Description: 
+ *		
+ *		The Chance class represents a chance deck along with the appropriate operations that can be done on that property.
+ */
+
+
+
+
+
