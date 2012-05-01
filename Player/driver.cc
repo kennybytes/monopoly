170dcc981ac3a45224a3001ab099a3f955df39c2
Kevin Hu <kevinhu@hawaii.edu>
2012-04-30 17:42:26 -1000

    added player initialization array

--- Player/driver.cc
+++ Player/driver.cc
@@ -8,6 +8,12 @@ using namespace std;
 
 main()
 {
+	int NumberPlayers = 3;
+	typedef Player PlayerArray[NumberPlayers];
+
+	PlayerArray PA;
+	Player::create_players(PA, NumberPlayers);
+/*
    Player A;
    cout <<"initial balance: " << A.get_balance()<<endl;
    cout << "initial position: "<< A.get_position()<<endl;  
@@ -21,6 +27,10 @@ main()
    A.update_position(4);
    cout << A.get_balance()<<endl;
    cout << A.get_position()<<endl; 
-
+*/
+	for(int i = 0; i < NumberPlayers; i++)
+	{	PA[i].update_balance(1000 + i);
+		cout << "Player " << i << " balance: " << PA[i].get_balance() << endl;
+	}
 
 }
