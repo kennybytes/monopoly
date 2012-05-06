32f6d3bed4ece667c297c842abda7f6eb1c5c816
Bryant Lee <blee4@hawaii.edu>
2012-05-05 19:06:49 -1000

    Added more files (place holder)

---
+++ GameSpaceTEST/deck.cc
@@ -0,0 +1,12 @@
+
+/* File: deck.cc
+ * Name: R4T5
+ * Date: 4 May 2012
+ * Rev: v0.1
+ */
+
+#include "deck.h"
+
+Deck :: Deck()
+{
+}
---
+++ GameSpaceTEST/deck.h
@@ -0,0 +1,17 @@
+
+/* File: deck.h
+ * Name: R4T5
+ * Date: 4 May 2012
+ * Rev: v0.1
+ */
+
+#ifndef GAMESPACE
+#define GAMESPACE
+
+class Deck : public GameSpace
+{
+	public:
+
+	private:
+
+};
